fef5409e3bb86dbfdf65920582aff483624300b9
Harish Brahme <harishbrahme@gmail.com>
2020-07-06 23:20:20 +0530

    add some files.

---
+++ site/dbcon.php
@@ -0,0 +1,8 @@
+<?php
+	$con=mysqli_connect('localhost','root','','mynew');
+
+	if ($con==false) {
+		echo "connection is not done";
+	}
+	
+?>
---
+++ site/signup1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Signup here</title>
+	<link rel="stylesheet" type="text/css" href="signup.css">
+</head>
+<body>
+<div class="form-wrap">
+	<form method="post" action="reg.php" >
+		<img src="images/avtar3.png" class="avtar">
+		<h1>SignUp Here</h1>
+		<input type="text" name="fname" placeholder="Enter Full name">
+		<input type="text" name="username" placeholder="Enter username">
+		<input type="text" name="mailid" placeholder="Enter mail ID">
+		<input type="Password" name="pass" placeholder="Enter Password">
+		<input type="Password" name="conpass" placeholder="Enter confirm Password">
+		<input type="submit" name="signup" value="Sign Up">
+		<a href="#">Lost Your Password</a><br>
+		<a href="newlog.html">Do you have an Account? Sign in</a>
+	</form>
+</div>
+</body>
+</html>
+
